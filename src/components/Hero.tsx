a7860928adfe0d5c1e6f8ba8843c118d35152814
Kikko Dev <kikko@example.com>
2026-08-05 12:49:14 +0100

    feat: Make Hero full screen dynamic height to hide menu below fold

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="relative flex items-center justify-center overflow-hidden bg-[#141A0F] border-b border-zinc-900 pt-16 pb-12 mt-20">
+    <section className="relative flex items-center justify-center overflow-hidden bg-[#141A0F] border-b border-zinc-900 min-h-[calc(100dvh-5rem)] py-8 mt-20">
       <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-zinc-800/20 via-[#141A0F] to-[#141A0F]"></div>
 
       {/* Content */}
